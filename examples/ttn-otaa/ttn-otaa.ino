edac874be68d5513c43afdf6af39a53af137d21b
Moritz Wirger <lj.moritorii@web.de>
2020-02-19 17:34:06 +0100

    Cleanup examples

--- examples/ttn-otaa/ttn-otaa.ino
+++ examples/ttn-otaa/ttn-otaa.ino
@@ -1,48 +1,55 @@
 #include <TTN_esp32.h>
+
 #include "TTN_CayenneLPP.h"
 
-//const char *devEui = "CHANGE_ME"; //changer avec de devEui TTN
-//const char *appEui = "CHANGE_ME"; // changer avec le appEui TTN
-//const char *appKey = "CHANGE_ME"; // changer avec le appKey TTN
+// const char *devEui = "CHANGE_ME"; // Change to TTN Device EUI
+// const char *appEui = "CHANGE_ME"; // Change to TTN Application EUI
+// const char *appKey = "CHANGE_ME"; // Chaneg to TTN Application Key
 
 TTN_esp32 ttn;
 TTN_CayenneLPP lpp;
 
-void message(const uint8_t *payload, size_t size, int rssi)
+void message(const uint8_t* payload, size_t size, int rssi)
 {
-	Serial.println("-- MESSAGE");
-	Serial.print("Received " + String(size) + " bytes RSSI=" + String(rssi) + "db");
-	for (int i = 0; i < size; i++)
-	{
-		Serial.print(" " + String(payload[i]));
-		//Serial.write(payload[i]);
-	}
+    Serial.println("-- MESSAGE");
+    Serial.print("Received " + String(size) + " bytes RSSI=" + String(rssi) + "db");
+    for (int i = 0; i < size; i++)
+    {
+        Serial.print(" " + String(payload[i]));
+        // Serial.write(payload[i]);
+    }
 
-	Serial.println();
+    Serial.println();
 }
 
-void setup() {
-	Serial.begin(115200);
-	Serial.println("Starting");
-	ttn.begin();
-	ttn.onMessage(message);// declare callback function when is downlink from server
-	ttn.join(devEui, appEui, appKey);
-	Serial.print("joining TTN ");
-	while (!ttn.joined()) {
-		Serial.print(".");
-		delay(500);
-	}
-	Serial.println("\njoined !");
-	ttn.showStatus();
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Starting");
+    ttn.begin();
+    ttn.onMessage(message); // Declare callback function for handling downlink
+                            // messages from server
+    ttn.join(devEui, appEui, appKey);
+    Serial.print("Joining TTN ");
+    while (!ttn.joined())
+    {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println("\njoined !");
+    ttn.showStatus();
 }
 
-void loop() {
-	static float nb = 18.2;
-	nb += 0.1;
-	lpp.reset();
-	lpp.addTemperature(1, nb);
-	if (ttn.sendBytes(lpp.getBuffer(), lpp.getSize())) {
-		Serial.printf("Temp: %f TTN_CayenneLPP: %d %x %02X%02X\n", nb, lpp.getBuffer()[0], lpp.getBuffer()[1], lpp.getBuffer()[2], lpp.getBuffer()[3]);
-	}
-	delay(10000);
+void loop()
+{
+    static float nb = 18.2;
+    nb += 0.1;
+    lpp.reset();
+    lpp.addTemperature(1, nb);
+    if (ttn.sendBytes(lpp.getBuffer(), lpp.getSize()))
+    {
+        Serial.printf("Temp: %f TTN_CayenneLPP: %d %x %02X%02X\n", nb, lpp.getBuffer()[0], lpp.getBuffer()[1],
+            lpp.getBuffer()[2], lpp.getBuffer()[3]);
+    }
+    delay(10000);
 }
--- examples/ttn_abp/ttn_abp.ino
+++ examples/ttn_abp/ttn_abp.ino
@@ -1,43 +1,48 @@
 #include <TTN_esp32.h>
+
 #include "TTN_CayenneLPP.h"
 
-const char *devAddr = "CHANGE_ME";
-const char *nwkSKey = "CHANGE_ME";
-const char *appSKey = "CHANGE_ME";
+// const char *devAddr = "CHANGE_ME"; // Change to TTN Device Address
+// const char *nwkSKey = "CHANGE_ME"; // Change to TTN Network Session Key
+// const char *appSKey = "CHANGE_ME"; // Change to TTN Application Session Key
 
 TTN_esp32 ttn;
 TTN_CayenneLPP lpp;
 
-void message(const uint8_t *payload, size_t size, int rssi)
+void message(const uint8_t* payload, size_t size, int rssi)
 {
-	Serial.println("-- MESSAGE");
-	Serial.print("Received " + String(size) + " bytes RSSI= " + String(rssi) + "dB");
+    Serial.println("-- MESSAGE");
+    Serial.print("Received " + String(size) + " bytes RSSI= " + String(rssi) + "dB");
 
-	for (int i = 0; i < size; i++)
-	{
-		Serial.print(" " + String(payload[i]));
-		//Serial.write(payload[i]);
-	}
+    for (int i = 0; i < size; i++)
+    {
+        Serial.print(" " + String(payload[i]));
+        // Serial.write(payload[i]);
+    }
 
-	Serial.println();
+    Serial.println();
 }
 
-void setup() {
-	Serial.begin(115200);
-	Serial.println("Starting");
-	ttn.begin();
-	ttn.onMessage(message);// declare callback function when is downlink from server
-	ttn.personalize(devAddr, nwkSKey, appSKey);
-	ttn.showStatus();
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Starting");
+    ttn.begin();
+    ttn.onMessage(message); // declare callback function when is downlink from server
+    ttn.personalize(devAddr, nwkSKey, appSKey);
+    ttn.showStatus();
 }
 
-void loop() {
-	static float nb = 18.2;
-	nb += 0.1;
-	lpp.reset();
-	lpp.addTemperature(1, nb);
-	if (ttn.sendBytes(lpp.getBuffer(), lpp.getSize())) {
-		Serial.printf("Temp: %f TTN_CayenneLPP: %d %x %02X%02X\n", nb, lpp.getBuffer()[0], lpp.getBuffer()[1], lpp.getBuffer()[2], lpp.getBuffer()[3]);
-	}
-	delay(10000);
+void loop()
+{
+    static float nb = 18.2;
+    nb += 0.1;
+    lpp.reset();
+    lpp.addTemperature(1, nb);
+    if (ttn.sendBytes(lpp.getBuffer(), lpp.getSize()))
+    {
+        Serial.printf("Temp: %f TTN_CayenneLPP: %d %x %02X%02X\n", nb, lpp.getBuffer()[0], lpp.getBuffer()[1],
+            lpp.getBuffer()[2], lpp.getBuffer()[3]);
+    }
+    delay(10000);
 }
