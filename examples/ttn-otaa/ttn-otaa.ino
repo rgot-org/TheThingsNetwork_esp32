90a8abf680a5f7fb491f3e3ce539b9ae30483524
Moritz Wirger <lj.moritorii@web.de>
2020-02-23 18:24:53 +0100

    Update examples

--- examples/ttn-otaa/ttn-otaa.ino
+++ examples/ttn-otaa/ttn-otaa.ino
@@ -2,14 +2,14 @@
 
 #include "TTN_CayenneLPP.h"
 /***************************************************************************
-*  Go to your TTN console register a device then the copy fields 
-*  and replace the CHANGE_ME strings below
-****************************************************************************/
-const char *devEui = "CHANGE_ME"; // Change to TTN Device EUI
-const char *appEui = "CHANGE_ME"; // Change to TTN Application EUI
-const char *appKey = "CHANGE_ME"; // Chaneg to TTN Application Key
+ *  Go to your TTN console register a device then the copy fields
+ *  and replace the CHANGE_ME strings below
+ ****************************************************************************/
+const char* devEui = "CHANGE_ME"; // Change to TTN Device EUI
+const char* appEui = "CHANGE_ME"; // Change to TTN Application EUI
+const char* appKey = "CHANGE_ME"; // Chaneg to TTN Application Key
 
-TTN_esp32 ttn;
+TTN_esp32* ttn = TTN_esp32::getInstance();
 TTN_CayenneLPP lpp;
 
 void message(const uint8_t* payload, size_t size, int rssi)
@@ -29,18 +29,18 @@ void setup()
 {
     Serial.begin(115200);
     Serial.println("Starting");
-    ttn.begin();
-    ttn.onMessage(message); // Declare callback function for handling downlink
-                            // messages from server
-    ttn.join(devEui, appEui, appKey);
+    ttn->begin();
+    ttn->onMessage(message); // Declare callback function for handling downlink
+                             // messages from server
+    ttn->join(devEui, appEui, appKey);
     Serial.print("Joining TTN ");
-    while (!ttn.joined())
+    while (!ttn->joined())
     {
         Serial.print(".");
         delay(500);
     }
     Serial.println("\njoined !");
-    ttn.showStatus();
+    ttn->showStatus();
 }
 
 void loop()
@@ -49,7 +49,7 @@ void loop()
     nb += 0.1;
     lpp.reset();
     lpp.addTemperature(1, nb);
-    if (ttn.sendBytes(lpp.getBuffer(), lpp.getSize()))
+    if (ttn->sendBytes(lpp.getBuffer(), lpp.getSize()))
     {
         Serial.printf("Temp: %f TTN_CayenneLPP: %d %x %02X%02X\n", nb, lpp.getBuffer()[0], lpp.getBuffer()[1],
             lpp.getBuffer()[2], lpp.getBuffer()[3]);
